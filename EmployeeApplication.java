f25f4d418c3b94282f478d71644a7b615995640f
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-07-12 18:33:06 -0300

    Add files via upload

---
+++ EmployeeApplication.java
@@ -0,0 +1,21 @@
+package com.journaldev.jaxrs.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class EmployeeApplication extends Application {
+	
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public EmployeeApplication() {
+		singletons.add(new EmployeeServiceImpl());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+
+}
